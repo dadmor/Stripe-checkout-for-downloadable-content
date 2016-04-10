7249a0c97b07c215bfc5cf46c661218255adbd2c
Grzegorz Durtan <dadmor@gmail.com>
2016-04-10 14:59:05 +0200

    prive veryfication

--- stripe-callback.php
+++ stripe-callback.php
@@ -3,7 +3,8 @@ require_once( 'stripe-lbraries/Stripe.php' );
 
 /* Your Secret Api key copy from your stripe user account */
 $_stripe_seccret_key = "sk_test_AylHsIQMTRhRU6jyfaGfdsD0";
-
+$_price_to_verify = 2900;
+$_currency = "eur";
 
 if ( $_POST  ) {
 
@@ -12,8 +13,8 @@ if ( $_POST  ) {
 		$success = '';
 		try {			
 			
-			Stripe_Charge::create(array("amount" => 3000,
-			"currency" => "eur",
+			Stripe_Charge::create(array("amount" => $_price_to_verify,
+			"currency" => $_currency,
 			"card" => $_POST['stripeToken'],
 			"description" => $_POST['stripeEmail']));
 			
